b6efc1a720bbf898457058e49302c82109bbefb0
iinaamasum <iinaamasum@gmail.com>
2022-05-02 10:07:34 +0600

    navbar fixed

--- src/Pages/HomeComponents/Home/Home.jsx
+++ src/Pages/HomeComponents/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import banner from '../../../images/image/robert-bye-tG36rvCeqng-unsplash.jpg';
 import Navbar from '../../Shared/Navbar/Navbar';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <div className="">
+        <img className="h-[90vh] w-full object-cover" src={banner} alt="" />
+      </div>
     </div>
   );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -20,13 +20,13 @@ const Navbar = () => {
   ];
   return (
     <>
-      <nav className="flex flex-wrap items-center justify-between px-2 bg-slate-200 sticky top-0 z-50 backdrop-opacity-20">
+      <nav className="flex flex-wrap items-center justify-between px-2 bg-slate-200 sticky top-0 z-50">
         <div
           style={{ maxWidth: '1300px' }}
           className="container px-4 lg:px-0 mx-auto flex flex-wrap items-center justify-between"
         >
-          <div className="w-full relative flex justify-between lg:w-auto lg:block lg:justify-start">
-            <Link to="/home" className="py-2 mr-4">
+          <div className="w-full relative flex justify-between lg:w-auto lg:block lg:items-center">
+            <Link to="/home" className="lg:w-1/3">
               <img className="h-14" src={logo} alt="" />
             </Link>
             <button
@@ -34,47 +34,49 @@ const Navbar = () => {
               type="button"
               onClick={() => setOpen(!open)}
             >
-              {!open ? <GiHamburgerMenu size={35} /> : <CgClose size={35} />}
+              {!open ? <GiHamburgerMenu size={30} /> : <CgClose size={30} />}
             </button>
           </div>
-          <div
-            className={`lg:flex flex-grow items-between justify-end
-              ${open ? 'flex' : 'hidden'}`}
-          >
-            <div className="lg:flex lg:items-center lg:justify-between w-full lg:w-auto">
-              <div className="w-full lg:w-auto text-center">
-                {navLinksOne.map((link) => (
-                  <Link
-                    className="block my-2 lg:my-0 text-center lg:inline-block mr-0 md:mr-6 font-semibold hover:text-red-800"
-                    to={link.path}
-                    key={link.id}
-                  >
-                    {link.name}
-                  </Link>
-                ))}
+          <div className="lg:flex lg:items-center lg:w-2/3 justify-end">
+            <div
+              className={`lg:flex flex-grow items-between
+              ${open ? 'block' : 'hidden'}`}
+            >
+              <div className="lg:flex lg:items-center lg:justify-between w-full lg:w-auto">
+                <div className="w-full lg:w-auto">
+                  {navLinksOne.map((link) => (
+                    <Link
+                      className="block my-2 lg:my-0 w-full lg:w-auto lg:inline-block mr-0 md:mr-6 font-semibold hover:text-red-800"
+                      to={link.path}
+                      key={link.id}
+                    >
+                      {link.name}
+                    </Link>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
 
-          <div
-            className={`lg:flex flex-grow items-between justify-end
-              ${open ? 'flex' : 'hidden'}`}
-          >
-            <button
-              onClick={() => navigate('/login')}
-              className="text-black my-2 lg:my-0 font-sans tracking-wider font-semibold text-md mr-3 hover:bg-gray-100 px-5 py-1 rounded-full hover:text-red-700 shadow hover:shadow outline-none focus:outline-none  mb-1 ease-linear transition-all duration-150 w-3/4 lg:w-auto"
-              type="button"
+            <div
+              className={`lg:flex flex-grow items-between
+              ${open ? 'block' : 'hidden'}`}
             >
-              Log In
-            </button>
-            <button
-              onClick={() => navigate('/signup')}
-              className="bg-orange-600 my-2 lg:my-0 text-white active:bg-red-800
-                  hover:bg-orange-800 font-bold  text-md px-5 py-1 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150 w-3/4 lg:w-auto tracking-wider mr-3"
-              type="button"
-            >
-              Sign Up
-            </button>
+              <button
+                onClick={() => navigate('/login')}
+                className="text-black my-2 lg:my-0 font-sans tracking-wider font-semibold text-md mr-3 hover:bg-gray-100 px-5 py-1 rounded-full hover:text-red-700 shadow hover:shadow outline-none focus:outline-none  mb-1 ease-linear transition-all duration-150 lg:w-auto"
+                type="button"
+              >
+                Log In
+              </button>
+              <button
+                onClick={() => navigate('/signup')}
+                className="bg-purple-600 my-2 lg:my-0 text-white active:bg-purple-800
+                  hover:bg-purple-800 font-bold  text-md px-5 py-1 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150 lg:w-auto tracking-wider mr-3"
+                type="button"
+              >
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
       </nav>
