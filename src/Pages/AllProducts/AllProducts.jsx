8cb43da674ff4657f25ad25887163cf17a5ffcde
iinaamasum <iinaamasum@gmail.com>
2022-05-02 20:28:49 +0600

    all product page updated with table

--- src/Pages/AllProducts/AllProducts.jsx
+++ src/Pages/AllProducts/AllProducts.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
-import SellingCard from '../HomeComponents/SellingCard/SellingCard';
+import ProductsTable from '../ProductsTable/ProductsTable';
 import Navbar from '../Shared/Navbar/Navbar';
 
 const AllProducts = () => {
@@ -12,10 +12,58 @@ const AllProducts = () => {
         <h2 className="text-4xl text-center text-rose-500 my-5 lg:my-10 font-bold">
           Available Products for Whole Sell
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-10">
-          {products.map((product) => (
-            <SellingCard key={product.id} product={product} />
-          ))}
+        <div class="flex flex-col mb-10">
+          <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+              <div class="overflow-hidden">
+                <table class="min-w-full">
+                  <thead class="border-b">
+                    <tr>
+                      <th
+                        scope="col"
+                        class="text-xl font-semibold text-gray-900 px-6 py-4 text-left"
+                      >
+                        Photo
+                      </th>
+                      <th
+                        scope="col"
+                        class="text-xl font-semibold text-gray-900 px-6 py-4 text-left"
+                      >
+                        Product Name
+                      </th>
+                      <th
+                        scope="col"
+                        class="text-xl font-semibold text-gray-900 px-6 py-4 text-left"
+                      >
+                        Supplier
+                      </th>
+                      <th
+                        scope="col"
+                        class="text-xl font-semibold text-gray-900 px-6 py-4 text-left"
+                      >
+                        Price
+                      </th>
+                      <th
+                        scope="col"
+                        class="text-xl font-semibold text-gray-900 px-6 py-4 text-left"
+                      >
+                        In Stock
+                      </th>
+                      <th
+                        scope="col"
+                        class="text-xl font-semibold text-gray-900 px-6 py-4 text-left"
+                      >
+                        Actions
+                      </th>
+                    </tr>
+                  </thead>
+                  {products.slice(0, 6).map((product) => (
+                    <ProductsTable key={product.id} product={product} />
+                  ))}
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
