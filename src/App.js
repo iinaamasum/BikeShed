11e870775954d815ddde0aae73ed27de645356a5
iinaamasum <iinaamasum@gmail.com>
2022-05-05 17:53:11 +0600

    hook form added with customization for add items

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import RequireAuth from './Pages/Auth/RequireAuth/RequireAuth';
 import SignUp from './Pages/Auth/SignUp/SignUp';
 import Error404 from './Pages/Error404/Error404';
 import Home from './Pages/HomeComponents/Home/Home';
+import AddItems from './Pages/LoggedUser/AddItems/AddItems';
+import AllItems from './Pages/LoggedUser/AllItems/AllItems';
+import ManageItems from './Pages/LoggedUser/ManageItems/ManageItems';
 import ProductUpdate from './Pages/ProductUpdate/ProductUpdate';
 import Footer from './Pages/Shared/Footer/Footer';
 
@@ -25,6 +28,31 @@ function App() {
             </RequireAuth>
           }
         />
+        <Route
+          path="/add-items"
+          element={
+            <RequireAuth>
+              <AddItems />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/manage-items"
+          element={
+            <RequireAuth>
+              <ManageItems />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/my-items"
+          element={
+            <RequireAuth>
+              <AllItems />
+            </RequireAuth>
+          }
+        />
+
         <Route path="/*" element={<Error404 />} />
       </Routes>
       <Footer />
---
+++ src/Pages/LoggedUser/AddItems/AddItems.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Navbar from '../../Shared/Navbar/Navbar';
+
+const AddItems = () => {
+  const { register, handleSubmit, errors } = useForm(); // initialize the hook
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <>
+      <Navbar />
+      <div style={{ maxWidth: '1300px' }} className="container mx-auto px-4">
+        <div className="bg-slate-200 my-10 lg:my16 w-full md:w-2/3 lg:w-1/2 mx-auto px-10 md:px-20 pt-5 pb-10 rounded">
+          <h2 className="text-center font-bold text-2xl md:text-3xl text-purple-700 mb-2">
+            Add Desired Items for Sell
+          </h2>
+          <h5 className="text-sm text-slate-600 my-2">
+            The Deller will help you to sell the products as soon as possible
+            for 2% of charge. So why are you waiting for. Please, fill up the
+            form quickly.
+          </h5>
+          <form onSubmit={handleSubmit(onSubmit)} className="">
+            <div>
+              <label className="text-sm font-medium mt-1" htmlFor="name">
+                Product Name
+              </label>
+              <br />
+              <input
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="Product Name"
+                {...register('name', { required: true, maxLength: 20 })}
+              />
+            </div>
+            <div>
+              <label className="text-sm font-medium mt-1" htmlFor="sup_name">
+                Supplier Name
+              </label>
+              <br />
+              <input
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="Supplier Name"
+                {...register('sup_name', {
+                  required: true,
+                })}
+              />
+            </div>
+            <div>
+              <label className="text-sm font-medium mt-1" htmlFor="des">
+                Description
+              </label>
+              <br />
+              <textarea
+                cols="30"
+                rows="3"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="Description"
+                {...register('des', { required: true })}
+              ></textarea>
+            </div>
+            <div>
+              <label className="text-sm font-medium mt-1" htmlFor="price">
+                Price
+              </label>
+              <br />
+              <input
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="Enter the price..."
+                type="number"
+                {...register('price', { required: true, min: 0 })}
+              />
+            </div>
+            <div>
+              <label className="text-sm font-medium mt-1" htmlFor="quantity">
+                Quantity
+              </label>
+              <br />
+              <input
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="Enter the quantity to sell..."
+                type="number"
+                {...register('quantity', { required: true, min: 0 })}
+              />
+            </div>
+            <div>
+              <label className="text-sm font-medium mt-1" htmlFor="img">
+                Image URL
+              </label>
+              <br />
+              <input
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                placeholder="Image URL"
+                type="text"
+                {...register('img')}
+              />
+            </div>
+            <input
+              className="w-full bg-purple-500 hover:bg-purple-700 text-center font-semibold text-white py-2 px-2 mt-3 rounded cursor-pointer"
+              type="submit"
+            />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddItems;
